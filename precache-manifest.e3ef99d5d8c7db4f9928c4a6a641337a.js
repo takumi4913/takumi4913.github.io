b5c243d6706583eaeb1d1d5cfaac7673414da761
takumi4913 <binfor4913@gmail.com>
2020-06-27 22:53:34 +0900

    Updates

---
+++ precache-manifest.e3ef99d5d8c7db4f9928c4a6a641337a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0cf83ea7d536f34b948e7c582c1f140",
+    "url": "/true/index.html"
+  },
+  {
+    "revision": "2bad452cc22531907605",
+    "url": "/true/static/css/main.a82193ae.chunk.css"
+  },
+  {
+    "revision": "6b2bad1fb909fd21d114",
+    "url": "/true/static/js/2.ccef9948.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/true/static/js/2.ccef9948.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bad452cc22531907605",
+    "url": "/true/static/js/main.7b5f7247.chunk.js"
+  },
+  {
+    "revision": "b41f6ca7c0f6a5dea3d3",
+    "url": "/true/static/js/runtime-main.cc12d387.js"
+  },
+  {
+    "revision": "c901ffc4a10c414b917c4ca71ddf6ede",
+    "url": "/true/static/media/20170706_2369764.c901ffc4.jpg"
+  },
+  {
+    "revision": "122dd47f2c18f5aa5396ccc94622f76d",
+    "url": "/true/static/media/University.122dd47f.png"
+  },
+  {
+    "revision": "d59d6198b513f61643dacaff33651f14",
+    "url": "/true/static/media/profile.d59d6198.png"
+  },
+  {
+    "revision": "e5981ee539ec4ad66847318058d27ad1",
+    "url": "/true/static/media/sidebar_true.e5981ee5.png"
+  },
+  {
+    "revision": "4d6fa813e5a21b9d176a8678ae558783",
+    "url": "/true/static/media/sidebar_true2.4d6fa813.png"
+  },
+  {
+    "revision": "dd73d60b2e2a5435ed6ffd8040685759",
+    "url": "/true/static/media/sidebar_true_side.dd73d60b.png"
+  }
+]);
